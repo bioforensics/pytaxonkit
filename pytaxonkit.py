b8fed47b5da4b2f9fc427401c681ff99c8cdc8ce
Daniel Standage <daniel.standage@nbacc.dhs.gov>
2021-07-09 20:09:36 -0400

    Fix doctest (#13)

--- pytaxonkit.py
+++ pytaxonkit.py
@@ -192,7 +192,7 @@ def list(ids, raw=False, threads=None, data_dir=None, debug=False):
     ...     subtaxa = [t for t in tree.traverse]
     ...     print(f'Top level result: {taxon.name} ({taxon.taxid}); {len(subtaxa)} related taxa')
     ...
-    Top level result: Solenopsis (13685); 293 related taxa
+    Top level result: Solenopsis (13685); 303 related taxa
     Top level result: Bos (9903); 29 related taxa
     >>> subtaxa[0]
     BasicTaxon(taxid=9904, rank='species', name='Bos gaurus')
