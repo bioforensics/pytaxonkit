090d74dfb6a650cee8a0a206b9bb08a5764f7f2d
Ryan <ryan.j.berger98@gmail.com>
2024-03-07 15:10:35 -0500

    Added --keep_invalid LCA flag

--- pytaxonkit.py
+++ pytaxonkit.py
@@ -1046,6 +1046,7 @@ def lca(
     multi=False,
     skip_deleted=False,
     skip_unfound=False,
+    keep_invalid=False,
     threads=None,
     data_dir=None,
     debug=False,
@@ -1063,6 +1064,8 @@ def lca(
         Ignore deleted taxids and compute LCA with the remaining taxa
     skip_unfound : bool, default False
         Ignore taxids not found in the taxonomy database and compute LCA with the remaining taxa
+    keep_invalid: bool, default False
+        Returns 0 when all taxids have been skipped from `skip_deleted` or `skip_unfound`
     threads : int
         Override the default taxonkit threads setting
     data_dir : str, default None
@@ -1096,6 +1099,8 @@ def lca(
         arglist.append("--skip-deleted")
     if skip_unfound:
         arglist.append("--skip-unfound")
+    if keep_invalid:
+        arglist.append("--keep-invalid")
     if threads:
         arglist.extend(("--threads", validate_threads(threads)))
     if data_dir:
@@ -1132,6 +1137,15 @@ def test_lca_unfound(capsys):
     assert lca([61021, 61022, 11111111], skip_unfound=True, debug=True) == 2628496
     terminal = capsys.readouterr()
     assert "taxonkit lca --skip-unfound" in terminal.err
+
+
+def test_lca_keep_invalid(capsys):
+    assert lca([11111111], skip_deleted=True, skip_unfound=True) == None
+    assert lca([22222222], skip_deleted=True, skip_unfound=True) == None
+    assert lca([11111111], skip_deleted=True, skip_unfound=True, keep_invalid=True) == 0
+    assert lca([11111111, 22222222], skip_deleted=True, skip_unfound=True, keep_invalid=True, debug=True) == 0
+    terminal = capsys.readouterr()
+    assert "taxonkit lca --skip-deleted --skip-unfound --keep-invalid" in terminal.err
 
 
 @pytest.mark.parametrize(
